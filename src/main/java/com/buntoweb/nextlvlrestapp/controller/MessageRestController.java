992611c5dc1d02cb7f2b67aec94008ec526ba839
Oleg Presniakov <ac6776@outlook.com>
2019-12-28 20:22:10 +0300

    some impro

--- src/main/java/com/buntoweb/nextlvlrestapp/controller/MessageRestController.java
+++ src/main/java/com/buntoweb/nextlvlrestapp/controller/MessageRestController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.function.BiConsumer;
 
+@CrossOrigin(origins = "http://localhost:8080", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class MessageRestController {
